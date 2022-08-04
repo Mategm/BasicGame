b17443e8dc83e687a632de5dbc6cb8659c686d7c
Mategm <mategm@gmail.com>
2022-08-04 13:35:16 +0200

    make basic methods

--- src/BasicGame.java
+++ src/BasicGame.java
@@ -1,15 +1,62 @@
 
 public class BasicGame {
+	
+	static int height = 10;
+	static int width = 10;
 
 	public static void main(String[] args) throws InterruptedException {
-		String[][] level = new String[10][10];
+		String[][] level = new String[height][width];
 		String playerMark = "O";
 		int row = 2;
 		int column = 2;
-		
 		Direction direction = Direction.RIGHT;
+		initLevel(level);
 		
-		// pálya inicializálása
+		for (int k=1; k <= 100; k++) {
+			
+			if (k % 10 == 0) {
+				direction = changeDirection(direction);
+			}
+			int[] coordinates = makeMove(direction, level, row, column);
+			row = coordinates[0];
+			column = coordinates[1];
+			draw(level, playerMark, row, column);
+			addSomeDelay(200L);
+		}
+	}
+
+	private static void addSomeDelay(long timeout) throws InterruptedException {
+		System.out.println("----------");
+		Thread.sleep(timeout);
+	}
+	
+	static int[] makeMove(Direction direction, String[][] level, int row, int column) {
+		switch (direction) {
+		case UP:
+			if (level[row-1][column].equals(" ")) {
+				row--;
+			}	
+			break;
+		case DOWN:
+			if (level[row+1][column].equals(" ")) {
+				row++;
+			}
+			break;
+		case LEFT:
+			if (level[row][column-1].equals(" ")) {
+				column--;
+			}
+			break;
+		case RIGHT:
+			if (level[row][column+1].equals(" ")) {
+				column++;
+			}
+			break;
+		}
+		return new int[] {row, column};
+	}
+
+	private static void initLevel(String[][] level) {
 		for (int i = 0; i < level.length; i++) {
 			for (int j = 0; j < level[i].length; j++) {
 				if (i == 0 || i == 9 || j == 0 || j == 9) {
@@ -18,42 +65,6 @@ public class BasicGame {
 					level[i][j] = " ";
 				}
 			}
-		}
-		
-		for (int k=1; k <= 100; k++) {
-			
-			if (k % 10 == 0) {
-				direction = changeDirection(direction);
-			}
-			
-			switch (direction) {
-			case UP:
-				if (level[row-1][column].equals(" ")) {
-					row--;
-				}	
-				break;
-			case DOWN:
-				if (level[row+1][column].equals(" ")) {
-					row++;
-				}
-				break;
-			case LEFT:
-				if (level[row][column-1].equals(" ")) {
-					column--;
-				}
-				break;
-			case RIGHT:
-				if (level[row][column+1].equals(" ")) {
-					column++;
-				}
-				break;
-			}
-			
-			
-			draw(level, playerMark, row, column);
-			
-			System.out.println("----------");
-			Thread.sleep(500L);
 		}
 	}
 
